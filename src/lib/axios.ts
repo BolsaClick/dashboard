d61bea6cff5a0dee84476304fe74d8db2cb646c9
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2025-03-26 13:16:55 -0300

    add proxy fix ip

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,23 @@
 // src/lib/axios.ts
 import axios from 'axios';
 
+const fixieUrl = new URL(process.env.FIXIE_URL!)
+
+
+
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, 
   headers: {
     'Content-Type': 'application/json',
     'accept': 'application/json', 
+  },
+  proxy: {
+    host: fixieUrl.hostname,
+    port: Number(fixieUrl.port),
+    auth: {
+      username: fixieUrl.username,
+      password: fixieUrl.password,
+    },
   },
 });
 
