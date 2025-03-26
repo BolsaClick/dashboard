3f106ce642a9587071fdcf8b6cf89c41a2c9a875
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2025-03-26 14:27:44 -0300

    fix test

--- src/pages/api/test-ip.ts
+++ src/pages/api/test-ip.ts
@@ -2,16 +2,15 @@ import axios from 'axios';
 import { HttpsProxyAgent } from 'https-proxy-agent';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+
+
+const proxyAgent = process.env.FIXIE_URL ? new HttpsProxyAgent(process.env.FIXIE_URL) : undefined;
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const fixieUrl = process.env.FIXIE_URL;
-    
-    if (!fixieUrl) {
-      return res.status(500).json({ error: 'FIXIE_URL não configurado corretamente' });
+    if (!process.env.FIXIE_URL) {
+      return res.status(500).json({ error: 'FIXIE_URL não configurado' });
     }
-
-    // Configurando o agente de proxy com a URL do Fixie
-    const proxyAgent = new HttpsProxyAgent(fixieUrl);
 
     // Usando Axios para realizar a requisição via proxy
     const response = await axios.get('https://api64.ipify.org?format=json', {
@@ -20,9 +19,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.status(200).json({ ip: response.data.ip });
   } catch (error) {
-    console.error('Erro ao obter o IP:', error);
-    const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-    return res.status(500).json({ error: 'Erro ao obter o IP', details: errorMessage });
+    return res.status(500).json({ error: 'Erro ao obter o IP' });
   }
 };
 
