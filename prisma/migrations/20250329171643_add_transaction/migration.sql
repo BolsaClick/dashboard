b7835c4a927177952cbcfa3291d3a210be91a1c4
Rodrigo Silverio <rodrigo.s.silverio@outlook.com>
2025-03-29 14:17:03 -0300

    add transaction

---
+++ prisma/migrations/20250329171643_add_transaction/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "transactions" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "UserStudent"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
